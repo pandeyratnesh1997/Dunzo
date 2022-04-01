66ff6f00de5fd5267a223588f60a2bdb1908d7b1
Rounak Abraham <rounakabraham@gmail.com>
2022-04-01 10:40:41 +0530

    update

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ function navbar(){
     return `<div id="menubar">
     <div id="menuLeft">
         <div><img src="https://resources.dunzo.com/web-assets/prod/_next/static/images/logo-7b2b540792556466133aea6e7c6ae513.png"></div>
-        <div class="menuTop"><select class="options"><option>Location</option></select></div>
+        <div class="menuTop"><i class="fa-solid fa-location-dot"></i><select class="options"><option>Location</option></select></div>
     </div>
     <div id="menuRight">
         <div id="search" class="hover"><i class="fa-solid fa-magnifying-glass icons"></i><p class="iconText">Search</p></div>
