a51abd4086ef920cb7e42517624187cb943b0eec
Rounak Abraham <rounakabraham@gmail.com>
2022-04-02 11:43:52 +0530

    update

--- components/navbar.js
+++ components/navbar.js
@@ -1,18 +1,48 @@
-function navbar(){
-    return `<div id="menubar">
+function navbar1(){
+    return `<div id="menubar123">
     <div id="menuLeft">
     <div><a href="index.html"><img src="https://resources.dunzo.com/web-assets/prod/_next/static/images/logo-7b2b540792556466133aea6e7c6ae513.png"></a></div>
-        <div class="menuTop"><i class="fa-solid fa-location-dot"></i><select class="options"><option>Mumbai</option></select></div>
+        <div class="menuTop"><i class="fa-solid fa-location-dot"></i><select class="options123"><option>Mumbai</option></select></div>
     </div>
     <div id="menuRight">
-        <div id="search" class="hover"><i class="fa-solid fa-magnifying-glass icons"></i><p class="iconText">Search</p></div>
+        <div id="search123" class="hover123"><i class="fa-solid fa-magnifying-glass icons123"></i><p class="iconText123">Search</p></div>
         <div id="cart" >
-            <div class="hover"><a href="cart.html"><i class="fa-solid fa-cart-shopping icons"></i><p class="iconText">Cart</p></a></div>
-            <div id="cartItems">0</div>
+            <div class="hover123"><a href="cart.html"><i class="fa-solid fa-cart-shopping icons123"></i><p class="iconText123">Cart</p></a></div>
+            <div id="cartItems123">0</div>
         </div>
-        <div id="profile" class="hover"><i class="fa-regular fa-user icons"></i><p class="iconText">Profile</p></div>
+        <div id="profile123" class="hover123"><a href="../DUNZOproject/profile.html"><i class="fa-regular fa-user icons123"></i><p class="iconText123">Profile</p></a></div>
     </div>
 </div>`
 }
 
-export default navbar;
+function navbar2(){
+    return `<div id="menubar123">
+    <div id="menuLeft">
+    <div><a href="index.html"><img src="https://resources.dunzo.com/web-assets/prod/_next/static/images/logo-7b2b540792556466133aea6e7c6ae513.png"></a></div>
+        <div class="menuTop"><i class="fa-solid fa-location-dot"></i><select class="options123"><option>Mumbai</option></select></div>
+    </div>
+    <div id="menuRight">
+        <div id="cart" >
+            <div class="hover123"><a href="cart.html"><i class="fa-solid fa-cart-shopping icons123"></i><p class="iconText123">Cart</p></a></div>
+            <div id="cartItems123">0</div>
+        </div>
+        <div id="profile123" class="hover123"><a href="../DUNZOproject/profile.html"><i class="fa-regular fa-user icons123"></i><p class="iconText123">Profile</p></a></div>
+    </div>
+</div>`
+}
+
+function navbar3(){
+    return `<div id="menubar123">
+    <div id="menuLeft">
+    <div><a href="index.html"><img src="https://resources.dunzo.com/web-assets/prod/_next/static/images/logo-7b2b540792556466133aea6e7c6ae513.png"></a></div>
+    </div>
+    <div id="menuRight">
+        <div id="cart" >
+            <div class="hover123"><a href="cart.html"><i class="fa-solid fa-cart-shopping icons123"></i><p class="iconText123">Cart</p></a></div>
+            <div id="cartItems123">0</div>
+        </div>
+    </div>
+</div>`
+}
+
+export {navbar1, navbar2, navbar3}
